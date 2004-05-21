7e67bc27ade5f4158cc755da3b0da8b13943fe1a
sclayman <sclayman@6c8f5b4b-bc4d-0410-9a3d-c887227c997f>
2004-05-21 11:20:23 +0000

    Classes not needed anymore.  These were protoype classes which have been refactored into newer classes.

--- timeindexing/com/timeindexing/io/ExternalIndexInteractor.java
+++
@@ -1,9 +0,0 @@
-// ExternalIndexInteractor.java
-
-package com.timeindexing.io;
-
-/**
- * An interface for readers and writers of external indexes.
- */
-public interface ExternalIndexInteractor extends InlineIndexInteractor { 
-}
--- timeindexing/com/timeindexing/io/InlineIndexInteractor.java
+++
@@ -1,9 +0,0 @@
-// InlineIndexInteractor.java
-
-package com.timeindexing.io;
-
-/**
- * An interface for readers and writers of inline indexes.
- */
-public interface InlineIndexInteractor extends InlineIndexReader, InlineIndexWriter { 
-}
