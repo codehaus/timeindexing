10b0c8cf03aaae29c02a0cb7ce93b0ac78b64805
sclayman <sclayman@6c8f5b4b-bc4d-0410-9a3d-c887227c997f>
2004-05-25 13:23:28 +0000

    Added DataType handling.

--- trunk/timeindexing/com/timeindexing/plugin/DefaultReaderResult.java
+++ trunk/timeindexing/com/timeindexing/plugin/DefaultReaderResult.java
@@ -3,7 +3,7 @@
 package com.timeindexing.plugin;
 
 import com.timeindexing.time.Timestamp;
-import java.nio.ByteBuffer;
+import com.timeindexing.index.DataType;import java.nio.ByteBuffer;
 
 /**
  * A default implementation for the value returned by a reader plugin.
@@ -11,13 +11,24 @@ import java.nio.ByteBuffer;
 public class DefaultReaderResult implements ReaderResult {
     ByteBuffer data = null;
     Timestamp dataTS = null;
+    DataType dataType = null;
 
+    /**
+     * Construct a DefaultReaderResult.
+     * The type will be DataType.NOTSET_DT
+     */
+    /*    public DefaultReaderResult(ByteBuffer bb, Timestamp dTS) {
+	data = bb;
+	dataTS = dTS;
+    }
+    */
     /**
      * Construct a DefaultReaderResult
      */
-    public DefaultReaderResult(ByteBuffer bb, Timestamp dTS) {
+    public DefaultReaderResult(ByteBuffer bb, Timestamp dTS, DataType dType) {
 	data = bb;
 	dataTS = dTS;
+	dataType = dType;
     }
 
     /**
@@ -34,4 +45,12 @@ public class DefaultReaderResult implements ReaderResult {
     public Timestamp getDataTimestamp() {
 	return dataTS;
     }
+
+    /**
+     * Get the DataType for this data
+     */
+    public DataType getDataType() {
+	return dataType;
+    }
+       
 }
