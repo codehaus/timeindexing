a8c4a8229527bcc00abbc3a39e9add6937e70686
sclayman <sclayman@6c8f5b4b-bc4d-0410-9a3d-c887227c997f>
2004-05-25 13:32:40 +0000

    Removed locate() and contains() code and put it in AbstractIndex.

--- timeindexing/com/timeindexing/index/DefaultIndexCache.java
+++ timeindexing/com/timeindexing/index/DefaultIndexCache.java
@@ -12,17 +12,12 @@ import com.timeindexing.basic.AbsolutePosition;
 import com.timeindexing.index.IndexTimestampSelector;
 import com.timeindexing.util.DoubleLinkedList;
 
-//import java.util.LinkedList;
-import java.util.TreeMap;
-import java.util.Comparator;
-
 /**
  * The default implementation of a cache which holds the index items.
  */
 public class DefaultIndexCache implements IndexCache {
-    int length = 0;
+    int cacheSize = 0;
     DoubleLinkedList indexItems = null;
-    TreeMap searchTree = null;
 
     Timestamp firstIndexTime = Timestamp.ZERO;
     Timestamp lastIndexTime = Timestamp.ZERO;
@@ -51,6 +46,9 @@ public class DefaultIndexCache implements IndexCache {
 	// add the IndexItem
 	indexItems.add(item);
 
+	// increase the cacheSize
+	cacheSize++;
+
 	Timestamp itemIndexTimestamp = item.getIndexTimestamp();
 	Timestamp itemDataTimestamp = item.getDataTimestamp();
 
@@ -69,20 +67,17 @@ public class DefaultIndexCache implements IndexCache {
 	lastIndexTime = itemIndexTimestamp;
 	lastDataTime = itemDataTimestamp;
 
-	// increase the length
-	length++;
 
-	// return the length
-	return length;
+	// return the cacheSize
+	return cacheSize;
 
     }
 
     /**
-     * Get the no of items in the index.
-     * TODO: put this in the header
+     * Get the no of items in the cache
      */
-    public synchronized long length() {
-	return length;
+    public synchronized long cacheSize() {
+	return cacheSize;
     }
 
     /**
@@ -94,7 +89,7 @@ public class DefaultIndexCache implements IndexCache {
 	} else {
 	    if (n < 0) {
 		throw new PositionOutOfBoundsException("Index value too low: " + n);
-	    } else if (n >= length()) {
+	    } else if (n >= cacheSize()) {
 		throw new PositionOutOfBoundsException("Index value too high: " + n);
 	    } else {
 		return (IndexItem)indexItems.get(n);
@@ -119,7 +114,7 @@ public class DefaultIndexCache implements IndexCache {
 	} else {
 	    if (n < 0) {
 		throw new PositionOutOfBoundsException("Index value too low: " + n);
-	    } else if (n >= length()) {
+	    } else if (n >= cacheSize()) {
 		throw new PositionOutOfBoundsException("Index value too high: " + n);
 	    } else {
 		return false;
@@ -134,166 +129,6 @@ public class DefaultIndexCache implements IndexCache {
     public boolean hollowItem(Position p) {
 	return hollowItem(p.value());
     }
-
-    /**
-     * Does a timestamp fall within the bounds of the Index.
-     * The bounds are the first time data is put in and the last
-     * time data is put in the Index.
-     */
-    public boolean contains(Timestamp t, IndexTimestampSelector selector) {
-	// if firstIndexTime <= t <= lastIndexTime
-	//   return true
-	// otherwise return false
-	if (selector == IndexTimestampSelector.DATA) {
-	    if (TimeCalculator.lessThanEquals(this.getFirstDataTime(), t) &&
-		TimeCalculator.lessThanEquals(t, this.getLastDataTime())) {
-		System.err.println("DataTS = " + t + " is contained in " +
-				   this.getFirstDataTime() + " to " +
-				   this.getLastDataTime());
-		return true;
-	    } else {
-		return false;
-	    }
-	} else {
-	    if (TimeCalculator.lessThanEquals(this.getFirstIndexTime(), t) &&
-		TimeCalculator.lessThanEquals(t, this.getLastIndexTime())) {
-		System.err.println("IndexTS = " + t + " is contained in " +
-				   this.getFirstIndexTime() + " to " +
-				   this.getLastIndexTime());
-		return true;
-	    } else {
-		return false;
-	    }
-	}
-    }
-
-
-    /**
-     * Try and determine the position associated
-     * with the speicifed Timestamp.
-     * @return null if no position is found
-     */
-    public TimestampMapping locate(Timestamp t, IndexTimestampSelector selector, Lifetime lifetime) {
-	System.err.println("DefaultIndexCache: locate: " + "TS = " + t);
-
-	if (! contains(t, selector)) { // timestamp t is not in this index
-	    // now try and determine if it is too low or too high
-	    if (selector == IndexTimestampSelector.DATA) {
-		if (TimeCalculator.lessThan(t, this.getFirstDataTime())) {
-		    return new TimestampMapping(t, Position.TOO_LOW);
-		} else if (TimeCalculator.greaterThan(t, this.getLastDataTime())) {
-		    return new TimestampMapping(t, Position.TOO_HIGH);
-		} else {
-		    throw new RuntimeException("DefaultIndexCache: locate failed to process timestamp " + t + ". First Data time = " + this.getFirstDataTime() + ". Last Data time = " + this.getLastDataTime() + ".");
-		}
-	    } else {
-		if (TimeCalculator.lessThan(t, this.getFirstIndexTime())) {
-		    return new TimestampMapping(t, Position.TOO_LOW);
-		} else if (TimeCalculator.greaterThan(t, this.getLastIndexTime())) {
-		    return new TimestampMapping(t, Position.TOO_HIGH);
-		} else {
-		    throw new RuntimeException("DefaultIndexCache: locate failed toprocess  timestamp " + t + ". First Index time = " + this.getFirstIndexTime() + ". Last Index time = " + this.getLastIndexTime() + ".");
-		}
-	    }
-	} else {
-	    // ensure the searchTree i set up
-	    if (searchTree == null) {
-		searchTree = new TreeMap(itemComparator);
-
-	    }
-	    // now search for the timestamp
-	    return binarySearch(t, 0, length()-1, selector, lifetime, 0);
-	}	
-    }
-
-
-    /**
-     * Do a binary search of the list.
-     */
-    protected TimestampMapping binarySearch(Timestamp t, long start, long end, IndexTimestampSelector selector, Lifetime lifetime, int depth) {
-	IndexItem item = null;
-	IndexItem itemN = null;
-
-	Timestamp itemTS = null;
-	Timestamp itemTSN = null;
-	
-
-	long halfway = (start + end) / 2;
-	long halfwayN = halfway+1;
-	AbsolutePosition halfwayPos = new AbsolutePosition(halfway);
-	AbsolutePosition halfwayPosNext = new AbsolutePosition(halfway+1);
-	
-	//System.err.print("binarySearch " + depth + ": " + t + "\t" + start + "\t" + end + "\t");
-
-	// try and find index item in search tree, if not found
-	// go and get it from list
-	if ((item = (IndexItem)searchTree.get(halfwayPos)) == null) {
-	    // position not in the tree
-	    // so get it from the index and put it in the tree
-	    item = getItem(halfway);
-	    searchTree.put(halfwayPos, item);
-	    //System.err.print("GET " + halfway + "\t");
-	} else {
-	    //System.err.print("TREE " + halfway + "\t");
-	}
-
-	// try and find index item in search tree, if not found
-	// go and get it from list
-	if ((itemN = (IndexItem)searchTree.get(halfwayPosNext)) == null) {
-	    // position not in the tree
-	    // so get it from the index and put it in the tree
-	    itemN = getItem(halfwayN);
-	    searchTree.put(halfwayPosNext, itemN);
-	    //System.err.print("GET " + halfwayN + "\t");
-	} else {
-	    //System.err.print("TREE " + halfwayN + "\t");
-	}
-
-	// get the relevant timestamps out of rge Index Items
-	if (selector == IndexTimestampSelector.DATA) {
-	    itemTS = item.getDataTimestamp();
-	    itemTSN =itemN.getDataTimestamp();
-	} else {
-	    itemTS = item.getIndexTimestamp();
-	    itemTSN = itemN.getIndexTimestamp();
-	}
-
-
-	//System.err.print(itemTS + "\t" + itemTSN);
-	//System.err.println();
-	    
-
-	if (TimeCalculator.greaterThanEquals(t, itemTS) &&
-	    TimeCalculator.lessThan(t, itemTSN)) {
-
-	    // if the timestamp is between itemTS and itemTSN
-	    // then we are close
-
-	    // the time is between two timestamps
-	    if (lifetime == Lifetime.CONTINUOUS) {
-		// if lifetimes are continuous then item has a lifetime
-		// from its own timestamp upto itemN's timestamp
-		// which means that item is the IndexItem to return
-		return new TimestampMapping(itemTS, item.getPosition());
-	    } else {
-		// if lifetimes are discrete then item's lifetime
-		// is a point in time and Timestamp t is after item,
-		// which means that itemN is the IndexItem to return
-		return new TimestampMapping(itemTSN,  itemN.getPosition());
-	    }
-	} else if (TimeCalculator.equals(t, itemTSN)) {
-	    // if the timestamp equals  itemTSN
-	    // we are there
-	    return new TimestampMapping(itemTSN,  itemN.getPosition());
-	} else if (TimeCalculator.lessThan(t, itemTS)) {
-	    // the timestamp is in first half, so search that half
-	    return binarySearch(t, start, halfway, selector, lifetime, depth+1);
-	} else {
-	    // the timestamp is in second half, so search that half
-	    return binarySearch(t, halfway, end, selector, lifetime, depth+1);
-	}
-    }
-
 
     /**
      * Get the time the first IndexItem was put into the Index.
@@ -323,28 +158,6 @@ public class DefaultIndexCache implements IndexCache {
 	return lastDataTime;
     }
 
-    private Comparator itemComparator = new Comparator() {
-	    public int compare(Object o1, Object o2) {
-		Position p1 = (Position)o1;
-		Position p2 = (Position)o2;
-		
-		long vp1 = p1.value();
-		long vp2 = p2.value();
-
-		if (vp1 < vp2) {
-		    return -1;
-		} else if (vp1 > vp2) {
-		    return 1;
-		} else { // vp1 == vp2
-		    return 0;
-		}
-	    }
-
-	    public boolean equals(Object obj) {
-		return super.equals(obj);
-	    }
-	};
-		
 		
 
 }
