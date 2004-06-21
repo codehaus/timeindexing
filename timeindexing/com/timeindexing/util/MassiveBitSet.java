6809d525246ee972377b2080545af2a96b02d853
sclayman <sclayman@6c8f5b4b-bc4d-0410-9a3d-c887227c997f>
2004-06-21 17:45:54 +0000

    First time

---
+++ timeindexing/com/timeindexing/util/MassiveBitSet.java
@@ -0,0 +1,155 @@
+// MassiveBitSet.java
+
+package com.timeindexing.util;
+
+import java.util.BitSet;
+
+/**
+ * A MassiveBitSet is used for appplications where a massive BitSet
+ * is required. 
+ * It is based on java.util.BitSet, but rather than
+ * resizing and copying an existing BitSet it uses a DoubleLinkedList
+ * of fixed size BitSets.
+ */
+public class MassiveBitSet {
+    // The size of the underlyinmg BitSet
+    static int SET_SIZE = 2048;
+
+    // The doubly linked list of BitSets
+    DoubleLinkedList linkedList = null;
+
+    /**
+     * Construct a MassiveBitSet
+     */
+    public MassiveBitSet() {
+	linkedList = new DoubleLinkedList();
+	allocate(1);
+    }
+
+    /**
+     * Construct a MassiveBitSet
+     * @param n the no of bits to pre-allocate
+     */
+    public MassiveBitSet(long n) {
+	linkedList = new DoubleLinkedList();
+	allocate(n);
+    }
+
+    /**
+     * Get the no of bits that can be currently stored
+     * in this MassiveBitSet.
+     */
+    public long size() {
+	return linkedList.size() * SET_SIZE;
+    }
+
+    /**
+     * Get the value of the nth bit.
+     * @param n the bit to get
+     */
+    public boolean get(long n) {
+	if (n > size()) {
+	    return false;
+	} else {
+	    // get the appropriate BitSet
+	    BitSet segment = (BitSet)linkedList.get(n / SET_SIZE);
+	    // get the bit
+	    boolean requiredBit = segment.get((int)(n % SET_SIZE));
+
+	    return requiredBit;
+	}
+    }
+
+    /**
+     * Clear the value of the nth bit.
+     * @param n the bit to get
+     */
+    public MassiveBitSet clear(long n) {
+	if (n > size()) {
+	    return this;
+	} else {
+	    // get the appropriate BitSet
+	    BitSet segment = (BitSet)linkedList.get((int)(n / SET_SIZE));
+	    // clear the bit
+	    segment.clear((int)(n % SET_SIZE));
+
+	    return this;
+	}
+    }
+
+    /**
+     * Clear all of the MassiveBitSet.
+     */
+    public MassiveBitSet clear() {
+	long listSize = linkedList.size();
+
+	long element=0; 
+
+	while (element < listSize) {
+	    BitSet aBitSet = (BitSet)linkedList.get(element);
+
+	    aBitSet.clear();
+	    element++;
+	}
+
+	return this;
+    }
+
+    /**
+     * Set the value of the nth bit.
+     * @param n the bit to get
+     */
+    public MassiveBitSet set(long n) {
+	return set(n, true);
+    }
+
+    /**
+     * Set the value of the nth bit.
+     * @param n the bit to get
+     * @param value the value to set bit n to
+     */
+    public MassiveBitSet set(long n, boolean value) {
+	if (n >= size()) {  // this MassiveBitSet isn't big enough to set bit n
+	    allocate(n);   // allocate enough space 
+	}
+
+	//System.err.println("Getting dll element " + (n / SET_SIZE) +  "/" + 
+	//		   linkedList.size() + " for bit " + n);
+
+	// get the appropriate BitSet
+	BitSet segment = (BitSet)linkedList.get((int)(n / SET_SIZE));
+	// set the bit
+	segment.set((int)(n % SET_SIZE), value);
+
+	return this;
+    }
+
+    /**
+     * Allocate enough space to hold up to n bits.
+     */
+    protected MassiveBitSet allocate(long n) {
+	if (n >= size()) {   // n really is > the highest bit we can hold
+	    // so work out how many BitSets we need
+	    long need = (n / SET_SIZE) + 1;
+	    long got = linkedList.size();
+
+	    long alloc=got; 
+
+	    //System.err.println("MassiveBitSet: allocate BitSet from " + got + " to " + need);
+
+	    while (alloc < need) {
+		linkedList.add(new BitSet(SET_SIZE));
+
+		alloc++;
+
+		//System.err.println("MassiveBitSet: allocated BitSet from " + got + " to " + alloc + " for bit " + n);
+
+	    }
+
+	    return this;
+	} else {
+	    // nothing to allocate
+	    return this;
+	}
+    }
+}
