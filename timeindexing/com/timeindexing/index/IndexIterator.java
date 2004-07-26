6517a0b8654f9e38cecd5d359f7edab8a8fdb233
sclayman <sclayman@6c8f5b4b-bc4d-0410-9a3d-c887227c997f>
2004-07-26 15:40:01 +0000

    First cut

---
+++ timeindexing/com/timeindexing/index/IndexIterator.java
@@ -0,0 +1,61 @@
+// IndexIterator.java
+
+package com.timeindexing.index;
+
+import java.util.Iterator;
+
+/**
+ * An iterator over the IndexItems in an Index.
+ */
+public class IndexIterator implements Iterator {
+    // The index being iterated over
+    Index index = null;
+
+    // The current position 
+    long position = 0;
+
+    /**
+     * Construct the Iterator given an Index.
+     */
+    protected IndexIterator(Index anIndex) {
+	index = anIndex;
+	position = 0;
+    }
+
+    /**
+     * Does the Iterator have another element.
+     */
+    public boolean hasNext() {
+	if (position < index.getLength()) {
+	    return true;
+	} else {
+	    return false;
+	}
+    }
+
+    /**
+     * Get the next element.
+     * @return null if it was not possible to get a particular IndexItem
+     */
+    public Object next() {
+	try {
+	    Object nextObject = index.getItem(position);
+
+	    position++;
+
+	    return nextObject;
+	} catch (GetItemException gie) {
+	    // it was not possible to get the item
+	    return null;
+	}
+    }
+
+    /**
+     * Remove an element.
+     * This is not supported, so an UnsupportedOperationExceptino is thrown
+     */
+    public void remove() {
+	throw new UnsupportedOperationException("Index Iterators do not support remove()");
+    }
+	
+}
