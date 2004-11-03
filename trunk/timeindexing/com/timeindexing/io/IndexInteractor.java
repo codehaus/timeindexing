74aa451650faebfbfcf854255c1be15c7cc458c7
sclayman <sclayman@6c8f5b4b-bc4d-0410-9a3d-c887227c997f>
2004-11-03 12:25:21 +0000

    Added getItem() and addItem() methods.

--- trunk/timeindexing/com/timeindexing/io/IndexInteractor.java
+++ trunk/timeindexing/com/timeindexing/io/IndexInteractor.java
@@ -3,6 +3,10 @@
 package com.timeindexing.io;
 
 import com.timeindexing.index.StoredIndex;
+import com.timeindexing.index.ManagedIndexItem;
+import com.timeindexing.basic.Position;
+
+import java.io.IOException;
 
 /**
  * An interface for objects that interact with indexes.
@@ -12,4 +16,19 @@ public interface IndexInteractor {
      * Get the index which this is doing I/O for.
      */
     public StoredIndex getIndex();
+
+    /**
+     * Get the item
+     * @param position the position of the index item to get
+     * @param withData read the data for this IndexItem if withData is true,
+     * the data needs to be read at a later time, otherwise
+     */
+    public ManagedIndexItem getItem(Position position, boolean withData) throws IOException;
+
+
+    /**
+     * Add an IndexItem to the  index.
+     */
+    public long addItem(ManagedIndexItem item) throws IOException;
+
 }
