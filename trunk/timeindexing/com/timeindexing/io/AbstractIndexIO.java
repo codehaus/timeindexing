031cacb96769c29e5d762f7ebb0f0b3c9529dab1
sclayman <sclayman@6c8f5b4b-bc4d-0410-9a3d-c887227c997f>
2004-11-03 12:30:20 +0000

    Added methods to start and stop Threads. Added queues for ead and write requests.

--- trunk/timeindexing/com/timeindexing/io/AbstractIndexIO.java
+++ trunk/timeindexing/com/timeindexing/io/AbstractIndexIO.java
@@ -3,10 +3,30 @@
 package com.timeindexing.io;
 
 import com.timeindexing.index.StoredIndex;
+import com.timeindexing.index.ManagedIndexItem;
+import com.timeindexing.basic.Position;
 
-public abstract class AbstractIndexIO implements IndexInteractor {
+import java.util.LinkedList;
+import java.io.IOException;
+
+/**
+ * An object for doing IO for an Index.
+ * It has a handle on the Index it's doing I/O for, and
+ * handles the thread for the I/O.
+ */
+public abstract class AbstractIndexIO implements IndexInteractor,  Runnable {
     // The index this is doing I/O for
     StoredIndex myIndex = null;
+
+    // The Thread for this I/O
+    Thread myThread = null;
+
+    // A work queue for read requests
+    LinkedList readQueue = null;
+
+
+    // A work queue for write requests
+    LinkedList writeQueue = null;
 
     /**
      * Get the index which this is doing I/O for.
@@ -14,4 +34,50 @@ public abstract class AbstractIndexIO implements IndexInteractor {
     public StoredIndex getIndex() {
 	return myIndex;
     }
+
+    /**
+     * Initialize the thread
+     * @param name the name of the thread
+     */
+    public Thread initThread(String name) {
+	myThread = new Thread(this, name);
+	readQueue = new LinkedList();
+	writeQueue = new LinkedList();
+	return myThread;
+    }
+
+
+    /**
+     * Get the thread
+     */
+    public Thread getThread() {
+	return myThread;
+    }
+
+    /**
+     * Start the thread
+     */
+    public Thread startThread() {
+	if (myThread != null) {
+	    myThread.start();
+	    //System.err.println("Started Thread " + myThread);
+	    return myThread;
+	} else {
+	    return null;
+	}
+    }
+
+    /**
+     * Stop the thread
+     */
+    public Thread stopThread() {
+	if (myThread != null) {
+	    myThread.stop();
+	    //System.err.println("Stopped Thread " + myThread);
+	    return myThread;
+	} else {
+	    return null;
+	}
+    }
+
 }
