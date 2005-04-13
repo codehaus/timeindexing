e9550867bc8e5dd3656d78c3ad4b0674cde71188
sclayman <sclayman@6c8f5b4b-bc4d-0410-9a3d-c887227c997f>
2005-04-13 22:14:59 +0000

    Changed arguments to format() to reflect changes in AbstractElapsedFormat. Changed other methods to reflect the signature change. Added code to deal with negative timestamps.

--- trunk/timeindexing/com/timeindexing/time/MicrosecondElapsedFormat.java
+++ trunk/timeindexing/com/timeindexing/time/MicrosecondElapsedFormat.java
@@ -12,6 +12,8 @@ import java.text.DecimalFormat;
  * e.g. <tt>00:00:01.593469</tt>
  */
 public class MicrosecondElapsedFormat extends AbstractElapsedFormat {
+    protected final long secsPerDay = 24 * 60 * 60;
+
     /*
      * A format for microseconds.  6 obligatory digits.
      */
@@ -26,58 +28,130 @@ public class MicrosecondElapsedFormat extends AbstractElapsedFormat {
     /**
      * Format a time using seconds, given seconds and nanoseconds.
      */
-    public String secondsFormat(long seconds, int nanoseconds) {
-	return seconds + "." + microsformat.format(nanoseconds/1000);
+    public String secondsFormat(RelativeTimestamp t) {
+	long seconds = Math.abs(t.getSeconds());
+	int nanoseconds = Math.abs(t.getNanoSeconds());
+	StringBuffer buffer = new StringBuffer(32);
+
+	if (t.isNegative()) {
+	    buffer.append("-");
+	}
+
+	buffer.append(seconds);
+	buffer.append(".");
+	buffer.append(microsformat.format(nanoseconds/1000));
+
+	return buffer.toString();
     }
 
 
     /**
      * Format a time for 1 hour interval, given seconds and nanoseconds.
      */
-    public String hourFormat(long seconds, int nanoseconds) {
+    public String hourFormat(RelativeTimestamp t) {
+	long seconds = Math.abs(t.getSeconds());
+	int nanoseconds = Math.abs(t.getNanoSeconds());
+
+	// some milliseconds needed for a Date object
 	long milliseconds = (seconds * 1000) + (nanoseconds / 1000000);
 	long microsOnly = nanoseconds/1000;
 
-	return firstHourformat.format((new Date(milliseconds))) + "." + microsformat.format(microsOnly);
+	StringBuffer buffer = new StringBuffer(32);
+
+	if (t.isNegative()) {
+	    buffer.append("-");
+	}
+
+	buffer.append(firstHourformat.format((new Date(milliseconds))));
+	buffer.append(".");
+	buffer.append(microsformat.format(microsOnly));
+
+	return buffer.toString();
     }
 
    /**
      * Format a time for 1 day interval, given seconds and nanoseconds.
      */
-    public String dayFormat(long seconds, int nanoseconds) {
+    public String dayFormat(RelativeTimestamp t) {
+	long seconds = Math.abs(t.getSeconds());
+	int nanoseconds = Math.abs(t.getNanoSeconds());
+
+	// some milliseconds needed for a Date object
 	long milliseconds = (seconds * 1000) + (nanoseconds / 1000000);
 	long microsOnly = nanoseconds/1000;
 
-	return firstDayformat.format((new Date(milliseconds))) + "." + microsformat.format(microsOnly);
+	StringBuffer buffer = new StringBuffer(32);
+
+	if (t.isNegative()) {
+	    buffer.append("-");
+	}
+
+	buffer.append(firstDayformat.format((new Date(milliseconds))));
+	buffer.append(".");
+	buffer.append(microsformat.format(microsOnly));
+
+	return buffer.toString();
     }
 
 
     /**
      * Format a time for 1 year interval, given seconds and nanoseconds.
      */
-    public String yearFormat(long seconds, int nanoseconds) {
+    public String yearFormat(RelativeTimestamp t) {
+	long seconds = Math.abs(t.getSeconds());
+	int nanoseconds = Math.abs(t.getNanoSeconds());
+
+	// some milliseconds needed for a Date object
 	long milliseconds = (seconds * 1000) + (nanoseconds / 1000000);
 	long microsOnly = nanoseconds/1000;
-	long secsPerDay = 24 * 60 * 60;
 
 	long days = seconds / secsPerDay;
 
-	return daysformat.format(days) + " " + firstYearformat.format((new Date(milliseconds))) + "." + microsformat.format(microsOnly);
+	StringBuffer buffer = new StringBuffer(32);
+
+	if (t.isNegative()) {
+	    buffer.append("-");
+	}
+
+	buffer.append(daysformat.format(days));
+	buffer.append(" ");
+	buffer.append(firstYearformat.format((new Date(milliseconds))));
+	buffer.append(".");
+	buffer.append(microsformat.format(microsOnly));
+
+	return buffer.toString();
     }
 
 
     /**
      * Format a time for any interval, given seconds and nanoseconds.
      */
-    public String fullFormat(long seconds, int nanoseconds) {
+    public String fullFormat(RelativeTimestamp t) {
+	long seconds = Math.abs(t.getSeconds());
+	int nanoseconds = Math.abs(t.getNanoSeconds());
+
+	// some milliseconds needed for a Date object
 	long milliseconds = (seconds * 1000) + (nanoseconds / 1000000);
 	long microsOnly = nanoseconds/1000;
-	long secsPerDay = 24 * 60 * 60;
 
 	long days = seconds / secsPerDay % 365;
 	long years = seconds / secsPerDay / 365;
 
-	return yearsformat.format(years) + " " +  daysformat.format(days) + " " +  catchAllformat.format((new Date(milliseconds))) + "." + microsformat.format(microsOnly);
+	StringBuffer buffer = new StringBuffer(32);
+
+	if (t.isNegative()) {
+	    buffer.append("-");
+	}
+
+	buffer.append(yearsformat.format(years));
+	buffer.append(" ");
+	buffer.append(daysformat.format(days));
+	buffer.append(" ");
+	buffer.append(catchAllformat.format((new Date(milliseconds))));
+	buffer.append(".");
+	buffer.append(microsformat.format(microsOnly));
+
+	return buffer.toString();
     }
 
 }
