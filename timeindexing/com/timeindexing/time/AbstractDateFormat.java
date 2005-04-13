ae82abb06e558a76b0cb0754a5b50376e45deb50
sclayman <sclayman@6c8f5b4b-bc4d-0410-9a3d-c887227c997f>
2005-04-13 22:56:00 +0000

    Added special hack, so that absolute dates in the first year since epoch, format as relative times.

--- timeindexing/com/timeindexing/time/AbstractDateFormat.java
+++ timeindexing/com/timeindexing/time/AbstractDateFormat.java
@@ -21,7 +21,14 @@ public abstract class AbstractDateFormat implements TimestampFormatting {
      * Format a Timestamp.
      */
     public String format(Timestamp t) {
-	return format((AbsoluteTimestamp)t);
+	// this is an experimental piece of code that
+	// converts AbsoluteTimestamps in the first year
+	// i.e. 1/1/1970 => 31/12/1970 into RelativeTimestamps
+	if (t.getSeconds() < (60 * 60 * 24 * 365)) {
+	    return TimeCalculator.toRelative((AbsoluteTimestamp)t).toString();
+	} else {
+	    return format((AbsoluteTimestamp)t);
+	}
     }
 
     /**
