55abbafd399929571d75d7c6a2d7ceff823222f9
sclayman <sclayman@6c8f5b4b-bc4d-0410-9a3d-c887227c997f>
2005-04-13 22:10:50 +0000

    Added definition of the Scale.

--- timeindexing/com/timeindexing/time/MicrosecondScale.java
+++ timeindexing/com/timeindexing/time/MicrosecondScale.java
@@ -8,4 +8,17 @@ import com.timeindexing.basic.Scale;
  * A time that is in the Microsecond Scale.
  */
 public interface MicrosecondScale extends Scale {
+    public MicrosecondScale SCALE = new MicrosecondScale() {
+	    /**
+	     * Scale is 10 ^ -6
+	     */
+	    public long value() {
+		return 1000000;
+	    }
+
+	    public String toString() {
+		return "Microsecond Scale";
+	    }
+
+	};
 }
--- timeindexing/com/timeindexing/time/SecondScale.java
+++ timeindexing/com/timeindexing/time/SecondScale.java
@@ -8,4 +8,17 @@ import com.timeindexing.basic.Scale;
  * A time that is in the Second Scale.
  */
 public interface SecondScale extends Scale {
+    public SecondScale SCALE = new SecondScale() {
+	    /**
+	     * Scale is 10 ^ 0
+	     */
+	    public long value() {
+		return 1;
+	    }
+
+	    public String toString() {
+		return "Second Scale";
+	    }
+
+	};
 }
