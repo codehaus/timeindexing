c7804301dfaab97c9729a047bada90bf18e5e577
sclayman <sclayman@6c8f5b4b-bc4d-0410-9a3d-c887227c997f>
2005-04-13 22:17:44 +0000

    First cut.

---
+++ trunk/timeindexing/uk/ti/TestConvert1.java
@@ -0,0 +1,175 @@
+package uk.ti;
+
+import com.timeindexing.basic.ID;
+import com.timeindexing.basic.UID;
+import com.timeindexing.index.Index;
+import com.timeindexing.index.IndexView;
+import com.timeindexing.index.IndexItem;
+import com.timeindexing.index.IndexType;
+import com.timeindexing.index.TimeIndexFactory;
+import com.timeindexing.index.TimeIndexException;
+import com.timeindexing.index.DataType;
+import com.timeindexing.index.IndexCreateException;
+import com.timeindexing.time.Timestamp;
+import com.timeindexing.time.MillisecondTimestamp;
+import com.timeindexing.time.ElapsedMillisecondTimestamp;
+import com.timeindexing.time.TimeCalculator;
+import com.timeindexing.data.DataItem;
+import com.timeindexing.data.StringItem;
+
+import java.util.GregorianCalendar;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+import java.util.Properties;
+import java.nio.ByteBuffer;
+
+/**
+ * First test of TimeIndexFactory and IncoreIndex.
+ */
+public class TestConvert1 {
+    public static void main(String [] args) {
+	GregorianCalendar calendar = new GregorianCalendar();
+
+	Properties createProperties = new Properties();
+	createProperties.setProperty("name", "testconvert1-incore");
+
+	TimeIndexFactory factory = new TimeIndexFactory();
+
+	try {
+
+	    IndexView index = factory.create(IndexType.INCORE_DT, createProperties);
+
+	    /* Item 0 */
+
+	    // ZERO timestamp
+	    Timestamp dTS = new ElapsedMillisecondTimestamp();
+	    // now
+	    Timestamp rTS = null;
+
+	    // A chunk of data
+	    DataItem data = null;
+
+
+	    /* Item 0 */
+	    data = new StringItem("quite a lot of stuff");
+
+	    rTS = new MillisecondTimestamp();
+
+	    index.addItem(data, dTS);
+
+	    delay(100);
+
+	    /* Item 1 */
+
+	    // work out elasped time
+
+	    data = new StringItem("on item 1");
+
+	    // work out elasped time
+	    dTS = TimeCalculator.elapsedSince(rTS);
+
+	    index.addItem(data, dTS);
+
+	    delay(100);
+
+	    /* Item 2 */
+
+	    data = new StringItem("this is the voice of the mysterons");
+
+	    // work out elasped time
+	    dTS = TimeCalculator.elapsedSince(rTS);
+
+	    index.addItem(data, dTS);
+
+	    /* A few more */
+	    for (int few = 0; few < 10; few++) {
+
+		int myDelay = 100 + (few * 10);
+
+		data = new StringItem("delay was " + myDelay);
+
+		delay(myDelay);
+
+		// work out elasped time
+		dTS = TimeCalculator.elapsedSince(rTS);
+
+		index.addItem(data, dTS);
+
+	    }
+
+	    delay(50);
+
+	    index.close();
+
+	    printIndex(index);
+
+	    System.err.println("converting........");
+
+	    Properties convertProperties = new Properties();
+	    convertProperties.setProperty("name", "testconvert1");
+	    convertProperties.setProperty("indexpath", "/tmp/testconvert1");
+
+	    Index convIndex = factory.save(index, IndexType.EXTERNAL_DT, convertProperties);
+
+	    convIndex.close();
+
+	    printIndex(convIndex);
+
+	    
+
+	} catch (TimeIndexException ice) {
+	    System.err.println("TestConvert1: " + ice.getMessage());
+	    System.exit(1);
+	}
+    }
+
+    public static void printIndex(Index index) throws TimeIndexException {
+	System.out.print("Name: " + index.getName() + "\n");
+	System.out.print("URI: " + index.getURI() + "\n");
+	System.out.print("Start:\t" + index.getStartTime() + "\n");
+	System.out.print("First:\t" + index.getFirstTime() + "\n");
+	System.out.print("Last:\t" + index.getLastTime() + "\n");
+	System.out.print("End:\t" + index.getEndTime() + "\n");
+
+	System.out.println("Length: " + index.getLength() + " items\n\n");
+
+	long total = index.getLength();
+	for (long i=0; i<total; i++) {
+	    IndexItem itemN = index.getItem(i);
+	    printIndexItem(itemN);
+	}
+	    
+	    
+    }
+
+    public static void printIndexItem(IndexItem item) throws TimeIndexException {
+
+	System.out.print(item.getDataTimestamp() + "\t");
+
+	System.out.print(item.getIndexTimestamp() + "\t");
+
+	ByteBuffer itemdata = item.getData();
+	if (item.getDataSize().value() > 16) {
+	    System.out.print(new String(itemdata.array()).substring(0,11) + "....\t");
+	} else {
+	    System.out.print(new String(itemdata.array()) + "\t");
+	}
+
+	System.out.print(item.getDataSize() + "\t");
+
+	System.out.print(item.getItemID() + "\t");
+
+	System.out.print(item.getAnnotations() + "\n");
+
+    }
+
+    public static void delay(long ms) {
+	try {
+	    Thread.sleep(ms);
+	} catch (java.lang.InterruptedException ie) {
+	    ;
+	}
+    }
+}
+
