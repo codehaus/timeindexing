7aec120ee577c5a4bc7f4ff82f8b2e41506e2165
sclayman <sclayman@6c8f5b4b-bc4d-0410-9a3d-c887227c997f>
2005-04-13 22:10:50 +0000

    Added definition of the Scale.

--- trunk/timeindexing/com/timeindexing/time/MillisecondScale.java
+++ trunk/timeindexing/com/timeindexing/time/MillisecondScale.java
@@ -8,4 +8,17 @@ import com.timeindexing.basic.Scale;
  * A time that is in the Millisecond Scale.
  */
 public interface MillisecondScale extends Scale {
+    public MillisecondScale SCALE = new MillisecondScale() {
+	    /**
+	     * Scale is 10 ^ -3
+	     */
+	    public long value() {
+		return 1000;
+	    }
+
+	    public String toString() {
+		return "Millisecond Scale";
+	    }
+	};
+
 }
--- trunk/timeindexing/com/timeindexing/time/NanosecondScale.java
+++ trunk/timeindexing/com/timeindexing/time/NanosecondScale.java
@@ -8,4 +8,17 @@ import com.timeindexing.basic.Scale;
  * A time that is in the Nanosecond Scale.
  */
 public interface NanosecondScale extends Scale {
+    public NanosecondScale SCALE = new NanosecondScale() {
+	    /**
+	     * Scale is 10 ^ -9
+	     */
+	    public long value() {
+		return 1000000000;
+	    }
+
+	    public String toString() {
+		return "Nanosecond Scale";
+	    }
+
+	};
 }
