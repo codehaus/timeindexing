fffaa6b322e876d49966c69fb415c59e7d625c30
sclayman <sclayman@6c8f5b4b-bc4d-0410-9a3d-c887227c997f>
2007-06-18 13:59:14 +0000

    Now uses a TreeMap for DataItems, plus there is a Comparator so items are sorted correctly.
    Access is now synchronized.
    
    Added increaseDataVolume and decreaseDataVolume methods.

--- trunk/timeindexing/com/timeindexing/cache/DefaultIndexCache.java
+++ trunk/timeindexing/com/timeindexing/cache/DefaultIndexCache.java
@@ -19,6 +19,9 @@ import com.timeindexing.index.DataAbstraction;
 import com.timeindexing.util.DoubleLinkedList;
 import com.timeindexing.util.MassiveBitSet;
 
+import java.util.TreeMap;
+import java.util.Comparator;
+
 /**
  * The default implementation of a cache which holds the index items.
  */
@@ -26,8 +29,8 @@ public class DefaultIndexCache implements IndexCache {
     // the size of the cache
     long cacheSize = 0;
 
-    // a doubly linked list of all the IndexItems
-    DoubleLinkedList indexItems = null;
+    // a tree of all the IndexItems
+    TreeMap indexItems = null;
 
     // a BitSet which says if an IndexItem is loaded or not
     MassiveBitSet loadedMask = null;
@@ -44,12 +47,32 @@ public class DefaultIndexCache implements IndexCache {
     Timestamp lastDataTime = Timestamp.ZERO;
     ManagedIndex myIndex = null;
 
+	Comparator itemComparator = new Comparator() {
+		public int compare(Object o1, Object o2) {
+		    Position p1 = (Position)o1;
+		    Position p2 = (Position)o2;
+
+		    long pos1 = p1.value();
+		    long pos2 = p2.value();
+
+
+		    if (pos1 < pos2) {
+			return -1;
+		    } else if (pos1 == pos2) {
+			return 0;
+		    } else {
+			return 1;
+		    }
+			    
+		}
+	    };
+
     /**
      * Create a DefaultIndexCache object.
      */
     public DefaultIndexCache(ManagedIndex index) {
 	myIndex = index;
-	indexItems = new DoubleLinkedList();
+	indexItems = new TreeMap(itemComparator);
 	loadedMask = new MassiveBitSet();
     }
 
@@ -77,7 +100,7 @@ public class DefaultIndexCache implements IndexCache {
     public synchronized long addItem(IndexItem item, long position) {
 	// create a new list if needed
 	if (indexItems == null) {
-	    indexItems = new DoubleLinkedList();
+	    indexItems = new TreeMap(itemComparator);
 	}
 
 	// call the policy
@@ -88,6 +111,8 @@ public class DefaultIndexCache implements IndexCache {
 	loadedMask.set(position);
 
 	// add the IndexItem to the rlevant position
+
+	/*
 	// if the doubly linked list is not big enough
 	// list cells are allocated to fill the gap
 	// TODO: it would be better to have a sparce data structure
@@ -102,6 +127,9 @@ public class DefaultIndexCache implements IndexCache {
 	    sparce(indexItems.size(), position-1);
 	    indexItems.add(item);
 	}
+	*/
+
+	indexItems.put(new AbsolutePosition(position), item);
 
 	// increase the cacheSize
 	cacheSize++;
@@ -149,7 +177,7 @@ public class DefaultIndexCache implements IndexCache {
     /**
      * Get an Index Item from the Index.
      */
-    public IndexItem getItem(long pos) {
+    public synchronized IndexItem getItem(long pos) {
 	if (indexItems == null) {
 	    return null;
 	} else {
@@ -158,7 +186,7 @@ public class DefaultIndexCache implements IndexCache {
 	    } else if (pos >= indexItems.size()) {
 		throw new PositionOutOfBoundsException("Index value too high: " + pos);
 	    } else {
-		IndexItem item = (IndexItem)indexItems.get(pos);
+		IndexItem item = (IndexItem)indexItems.get(new AbsolutePosition(pos));
 
 		// call the policy
 		if (policy != null) {
@@ -178,7 +206,7 @@ public class DefaultIndexCache implements IndexCache {
     /**
      * Get an Index Item from the Index.
      */
-    public IndexItem getItem(Position p) {
+    public synchronized IndexItem getItem(Position p) {
 	return getItem(p.value());
     }
 
@@ -187,7 +215,7 @@ public class DefaultIndexCache implements IndexCache {
      * Contains the IndexItem at the speicifed position.
      * If the cache contains the item, it means it is loaded.
      */
-    public boolean containsItem(long pos) {
+    public synchronized boolean containsItem(long pos) {
 	return loadedMask.get(pos);
     }
 
@@ -196,7 +224,7 @@ public class DefaultIndexCache implements IndexCache {
      * Contains the IndexItem at the speicifed position.
      * If the cache contains the item, it means it is loaded.
      */
-    public boolean containsItem(Position p) {
+    public synchronized boolean containsItem(Position p) {
 	return containsItem(p.value());
     }
 	    
@@ -204,7 +232,7 @@ public class DefaultIndexCache implements IndexCache {
      * Hollow the IndexItem at the position.
      * This does nothing by default as the data will be lost.
      */
-    public boolean hollowItem(long pos) {
+    public synchronized boolean hollowItem(long pos) {
 	if (indexItems == null) {
 	    return false;
 	} else {
@@ -222,7 +250,7 @@ public class DefaultIndexCache implements IndexCache {
     /**
      * Hollow the IndexItem at the position.
      */
-    public boolean hollowItem(Position p) {
+    public synchronized boolean hollowItem(Position p) {
 	return hollowItem(p.value());
     }
 
@@ -231,7 +259,7 @@ public class DefaultIndexCache implements IndexCache {
      * Remove the IndexItem at the speicifed position.
      * This does nothing by default as the data will be lost.
      */
-    public boolean removeItem(long pos) {
+    public synchronized boolean removeItem(long pos) {
 	if (indexItems == null) {
 	    return false;
 	} else {
@@ -248,7 +276,7 @@ public class DefaultIndexCache implements IndexCache {
     /**
      * Remove the IndexItem at the speicifed position.
      */
-    public boolean removeItem(Position p) {
+    public synchronized boolean removeItem(Position p) {
 	return removeItem(p.value());
     }
 
@@ -267,6 +295,25 @@ public class DefaultIndexCache implements IndexCache {
 	return volumeHeld;
     }
 
+    /**
+     * Increase the data volume
+     * @return the new data volume
+     */
+    public long increaseDataVolume(long v) {
+	volumeHeld += v;
+	return volumeHeld;
+    }
+    
+
+    /**
+     * Decrease the data volume
+     * @return the new data volume
+     */
+    public long decreaseDataVolume(long v) {
+	volumeHeld -= v;
+	return volumeHeld;
+    }
+    
 
     /**
      * Get the time the first IndexItem was put into the Index.
@@ -300,7 +347,7 @@ public class DefaultIndexCache implements IndexCache {
      * Set the cache policy.
      * @return the old cache policy
      */
-    public CachePolicy setPolicy(CachePolicy pol) {
+    public synchronized CachePolicy setPolicy(CachePolicy pol) {
 	CachePolicy oldPolicy = policy;
 
 	policy = pol;
@@ -312,13 +359,14 @@ public class DefaultIndexCache implements IndexCache {
     /**
      * Get the current cache policy.
      */
-    public CachePolicy getPolicy() {
+    public synchronized CachePolicy getPolicy() {
 	return policy;
     }
 
     /**
      * Create some sparce elements in the cache.
      */
+    /*
     protected long sparce(long start, long end) {
 	long  current = start;
 
@@ -331,6 +379,6 @@ public class DefaultIndexCache implements IndexCache {
 	}
 
 	return current;
-    }
+	}*/
 
 }
