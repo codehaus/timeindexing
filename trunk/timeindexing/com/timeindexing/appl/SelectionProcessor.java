cd7f9fe5bb31466e004837b4a9b7dcdc3f9df735
sclayman <sclayman@6c8f5b4b-bc4d-0410-9a3d-c887227c997f>
2007-06-18 13:31:52 +0000

    Now uses IndexPropertiesProcessor to process the properties.

--- trunk/timeindexing/com/timeindexing/appl/SelectionProcessor.java
+++ trunk/timeindexing/com/timeindexing/appl/SelectionProcessor.java
@@ -59,142 +59,20 @@ public class SelectionProcessor {
      * It will ignore properties not relevant to the selection.
      */
     public IndexView select(IndexView index, IndexProperties properties, Lifetime lifetime) {
-	AbsolutePosition startPos = null;
-	Position endPos = null;
-	AbsoluteTimestamp startTime = null;
-	boolean useStartPos = false;
-	Interval interval = null;
+    	Interval interval = null;
 	IndexView selection = null;
-	PositionParser positionParser = new PositionParser();
-	CountParser countParser = new CountParser();
-	TimeDateParser timeParser = new TimeDateParser();
+	IndexPropertiesProcessor ipp = new IndexPropertiesProcessor();
 
 	/*
-	 * Determine start of selection.
+	 * Determine interval for selection.
 	 */
-	if (properties.containsKey("startpos")) {
-	    // convert string to position object
-	    startPos = positionParser.parse((String)properties.get("startpos"));
-	    if (startPos == null) {
-		startPos = new AbsolutePosition(0);
-	    }
+    	interval = ipp.createInterval(index, properties);
 
-	    useStartPos = true;
-
-	} else if (properties.containsKey("starttime")) {
-	    // convert string to time object
-	    startTime = (AbsoluteTimestamp)timeParser.parse((String)properties.get("starttime"), true);
-	    if (startTime == null) {
-		startPos = new AbsolutePosition(0);
-		useStartPos = true;
-	    } else {
-		useStartPos = false;
-	    }
-
-
-	} else {
-	    startPos = new AbsolutePosition(0);
-	    useStartPos = true;
-	}
-	    
-	/*
-	 * Determine end of selection.
-	 * We already know something about the start of the selection.
-	 */
-	if (properties.containsKey("endpos")) {
-	    // convert string to position object
-	    endPos = positionParser.parse((String)properties.get("endpos"));
-	    if (endPos == null) {
-		endPos = new AbsolutePosition(index.getLength());
-	    }
-
-	    if (useStartPos) {
-		interval = new EndPointInterval(startPos, endPos);
-	    } else {
-		interval = new EndPointInterval(startTime, endPos);
-	    }
-
-	} else if (properties.containsKey("endtime")) {
-	    // convert string to time object
-	    AbsoluteTimestamp endTime = (AbsoluteTimestamp)timeParser.parse((String)properties.get("endtime"), true);
-	    if (endTime == null) {
-		endPos = new AbsolutePosition(index.getLength());
-	    }
-
-	    if (useStartPos) {
-		if (endTime == null) {
-		    interval = new EndPointInterval(startPos, endPos);
-		} else {
-		    interval = new EndPointInterval(startPos, endTime);
-		} 
-	    } else {
-		if (endTime == null) {
-		    interval = new EndPointInterval(startTime, endPos);
-		} else {
-		    interval = new EndPointInterval(startTime, endTime);
-		}
-	    }
-
-
-	} else if (properties.containsKey("count")) {
-	    // convert string to count object
-	    Count count = countParser.parse((String)properties.get("count"));
-
-	    if (count == null) {
-		endPos = new AbsolutePosition(index.getLength());
-	    }
-
-	    if (useStartPos) {
-		if (count == null) {
-		    interval = new EndPointInterval(startPos, endPos);
-		} else {
-		    interval = new EndPointInterval(startPos, count);
-		} 
-	    } else {
-		if (count == null) {
-		    interval = new EndPointInterval(startTime, endPos);
-		} else {
-		    interval = new EndPointInterval(startTime, count);
-		}
-	    }
-
-
-
-	} else if (properties.containsKey("for")) {
-	    // convert string to relative time object
-	    RelativeTimestamp elapsed = (RelativeTimestamp)timeParser.parse((String)properties.get("for"), false);
-	    if (elapsed == null) {
-		endPos = new AbsolutePosition(index.getLength());
-	    }
-
-	    if (useStartPos) {
-		if (elapsed == null) {
-		    interval = new EndPointInterval(startPos, endPos);
-		} else {
-		    interval = new EndPointInterval(startPos, elapsed);
-		} 
-	    } else {
-		if (elapsed == null) {
-		    interval = new EndPointInterval(startTime, endPos);
-		} else {
-		    interval = new EndPointInterval(startTime, elapsed);
-		}
-	    }
-
-
-	} else {
-	    endPos = new AbsolutePosition(index.getLength());
-
-	    if (useStartPos) {
-		interval = new EndPointInterval(startPos, endPos);
-	    } else {
-		interval = new EndPointInterval(startTime, endPos);
-	    }
-
-	}
-	   
 	// select from the interval
 	selection = index.select(interval, IndexTimestampSelector.DATA, Overlap.FREE, lifetime);
+
+	System.err.println(Clock.time.time() + " " + index.getURI() + "." + ipp.prettyPrint(properties) + " Items = " + selection.getLength() + ". Thread " + Thread.currentThread().getName() );
+
 
 	return selection;
     }
