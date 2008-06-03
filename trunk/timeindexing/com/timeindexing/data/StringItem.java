26aacd65c32b6f5b1fb2a304571e244928b46393
sclayman <sclayman@6c8f5b4b-bc4d-0410-9a3d-c887227c997f>
2008-06-03 14:22:49 +0000

    This now extends AbstractDataItem. It can convert a String into a ByteBuffer, and a ByteBuffer into a String.

--- trunk/timeindexing/com/timeindexing/data/StringItem.java
+++ trunk/timeindexing/com/timeindexing/data/StringItem.java
@@ -10,31 +10,52 @@ import com.timeindexing.index.DataType;
  * A item of data presented by a data reader
  * This is an implementation of StringItem.
  */
-public class StringItem implements DataItem {
+public class StringItem extends AbstractDataItem implements DataItem {
     /*
      * The string
      */
-    ByteBuffer stringBuffer = null;
+    String string = null;
+
+    /*
+     * The ByteBuffer
+     */
+    ByteBuffer theBuffer = null;
 
     /**
      * Construct a StringItem from a string.
      */
     public StringItem(String s) {
-	stringBuffer = ByteBuffer.wrap(s.getBytes());
+	string = s;
+    }
+
+    /**
+     * Create a StringItem from a ByteBuffer.
+     * This is used internally by DataItemFactory.
+     */
+    StringItem(ByteBuffer b) {
+	theBuffer = b;
     }
 
     /**
      * Get the data itself
      */
     public ByteBuffer getBytes() {
-	return stringBuffer;
+	if (theBuffer == null) {
+	    construct();
+	}
+
+	return theBuffer;
     }
 
     /**
      * Get the size of the item
      */
     public long getSize() {
-	return stringBuffer.limit();
+	if (theBuffer == null) {
+	    construct();
+	}
+
+	return theBuffer.limit();
     }
 
     /**
@@ -42,5 +63,40 @@ public class StringItem implements DataItem {
      */
     public DataType getDataType() {
 	return DataType.STRING;
+    }
+
+    /**
+     * Get the String object from this StringItem.
+     * @return a String
+     */
+    public Object getObject() {
+	if (string == null) {
+	    deconstruct();
+	}
+
+	return string;
+    }
+
+    /**
+     * Construct the ByteBuffer from the String.
+     */
+    private void construct() {
+	theBuffer = ByteBuffer.wrap(string.getBytes());
+    }
+
+    /**
+     * Deconstruct the ByteBuffer into a String
+     */
+    private void deconstruct() {
+	// We need to construct a byte[] of the right size,
+	// and extract the ByteBuffer into the byte[].
+	// The we can construct a String around the byte[].
+	int len = (int)getSize();
+
+	byte[] theBytes = new byte[len];
+	theBuffer.get(theBytes);
+
+	string = new String(theBytes);
+	
     }
 }
